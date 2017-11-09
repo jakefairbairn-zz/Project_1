21985c7186423e054b1b11e16de1984e601c2c99
Jake Fairbairn <jake_fairbairn@byu.net>
2017-11-09 10:42:53 -0700

    Add project files.

---
+++ Project_1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Contact()
+        {
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            return View();
+        }
+
+        public ViewResult Mission()
+        {
+            return View();
+        }
+    }
+}
