be81ed72fc30f5a7a141151f9190a3f5b81c7cbe
Jake Fairbairn <jake_fairbairn@byu.net>
2017-11-13 18:55:05 -0700

    Added question form and reply options to Mission Info page

---
+++ Project_1/Models/QuestionForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_1.Models
+{
+    public class QuestionForm
+    {
+        public string Question { get; set; }
+    }
+}
